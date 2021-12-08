4ee717cea29a74ba70716dc5646816823efe9027
Aleksandr <haus.esc@gmail.com>
2021-12-08 23:38:52 +0300

    final

---
+++ yatube/posts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post, Group
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)  
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
