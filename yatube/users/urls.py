40502868d3638c7bbb366aa9795972ca89aa27d3
Aleksandr <haus.esc@gmail.com>
2021-12-17 22:17:34 +0300

    final1701

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -5,9 +5,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-  path('logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-      ),
-  path('signup/', views.SignUp.as_view(), name='signup')
+    path('logout/',
+         LogoutView.as_view(template_name='users/logged_out.html'),
+         name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
