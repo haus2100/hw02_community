18cdbd31a8b32210546a20228535cd2d3c78026c
Aleksandr <haus.esc@gmail.com>
2021-12-17 18:05:02 +0300

    final1701

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all()[:10]
     context = {
         'posts': posts,
     }
@@ -14,7 +14,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
 
-    posts = group.group_posts.all()
+    posts = group.group_posts.all()[:10]
     context = {
         'group': group,
         'posts': posts,
